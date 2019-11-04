9831654fbf5d4244c2251d3b8cc51531f3271010
Rebelo <Luis_Rebelo@Dellteam.com>
2019-11-04 15:21:53 -0300

    Project uploaded to git

---
+++ src/test/java/com/spring10steps/spring10steps/Spring10stepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring10steps.spring10steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring10stepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
